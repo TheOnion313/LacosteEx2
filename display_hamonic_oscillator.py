6c15cafdc03aa3cfd16ce1f15c0e891676416fb6
TheOnion313 <royn77777@gmail.com>
2023-04-08 19:08:56 +0300

    Added log-log graph to show different conversion rates

--- display_hamonic_oscillator.py
+++ display_hamonic_oscillator.py
@@ -1,20 +1,35 @@
 from matplotlib import pyplot as plt
 import pickle as p
+import numpy as np
+from math import log
 
 
 def main():
     with open("harmonic_os.p", "rb") as f:
         archive = p.load(f)
-        dt_archive, x_archive_euler = archive["dt"], archive["x_euler"]
-        x_archive_midpoint = archive["x_midpoint"]
-        x_archive_rk4 = archive["x_rk4"]
+        dt_archive, x_archive_euler = np.array(archive["dt"]), np.array(archive["x_euler"])
+        x_archive_midpoint = np.array(archive["x_midpoint"])
+        x_archive_rk4 = np.array(archive["x_rk4"])
 
-    plot = plt.subplot()
-    plot.plot(dt_archive, x_archive_euler)
-    plot.plot(dt_archive, x_archive_midpoint)
-    plot.plot(dt_archive, x_archive_rk4)
-    plot.legend(["euler", "midpoint", "rk4"])
-    plot.invert_xaxis()
+    dt_log = [log(d) for d in dt_archive]
+    euler_log = [log(d) for d in x_archive_euler]
+    midpoint_log = [log(d) for d in x_archive_midpoint]
+    rk4_log = [log(abs(d)) if d != 0 else -1000 for d in x_archive_rk4]
+
+    graph, (plot1, plot2) = plt.subplots(1, 2)
+    plot1.plot(dt_archive, x_archive_euler)
+    plot1.plot(dt_archive, x_archive_midpoint)
+    plot1.plot(dt_archive, x_archive_rk4)
+    plot1.legend(["euler", "midpoint", "rk4"])
+
+    plot1.invert_xaxis()
+
+    plot2.plot(dt_log, euler_log)
+    plot2.plot(dt_log, midpoint_log)
+    plot2.plot(dt_log, rk4_log)
+    plot2.legend(["euler_log", "midpoint_log", "rk4_log"])
+
+    plot2.invert_xaxis()
 
     plt.show()
 
